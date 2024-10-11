ed7648d9c73261acdfca044305d8900f4c6eaa13
TrujiDev <eetgrisales@gmail.com>
2024-10-11 10:37:49 -0500

    Add initial project configuration files
    - Include .gitignore to exclude unnecessary files\n- Add Maven wrapper scripts (mvnw and mvnw.cmd)\n- Include pom.xml for project dependencies and build settings\n- Add test source directory (src/test/) for unit tests\n- Add Maven directory (.mvn/) for wrapper and settings

---
+++ src/test/java/trujidev/fitness_zone/FitnessZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package trujidev.fitness_zone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
